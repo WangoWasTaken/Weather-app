062c29c989676755904c1a98b62454e6e01b6615
WangoWasTaken <70453739+WangoWasTaken@users.noreply.github.com>
2023-06-15 13:56:17 +0300

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -14,14 +14,21 @@ while number <= 5:
     number += 1
     temp=(random.randint(15,27))
     weather_forecast.append(temp)
-    print(temp)
+    time=(random.randint(0,23))
     weather_forecast_size = len(weather_forecast)
-    if weather_forecast_size <= 1:
-        print("Today's temperature has been:", *weather_forecast)
+    sleep(0.1)
+    time_forecast.update( {weather_forecast[-1] : time})
+
+
+for key, value in time_forecast.items():
+    print("The temperature was", key, "degrees at",value, "o'clock")
+
+
+
+while True:
+    question = int(input("How many degrees is it: "))
+    if question in weather_forecast:
+        print("Yay correct")
+        break
     else:
-        print("Today's temperatures have been:", *weather_forecast, sep=", ")
-    sleep(1)
-    time_forecast.update( {weather_forecast[-1] : "2am"})
-    print(time_forecast)
-
-
+        print("Guess again")
