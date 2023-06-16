a8360e95c15071761dccd9526192be13d104b19a
WangoWasTaken <70453739+WangoWasTaken@users.noreply.github.com>
2023-06-16 13:04:57 +0300

    Add files via upload

--- weather_app.py
+++ weather_app.py
@@ -1,34 +1,57 @@
 import random
 from time import sleep
 
-number = 0
 weather_forecast = []
-
 time_forecast = {
 
 }
 
+clock = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
 
+i = 0
 
-while number <= 5:
-    number += 1
-    temp=(random.randint(15,27))
+temp = (random.randint(4,9))
+
+while i < len(clock):
+    print(clock[i])
+    i = i +1
+    prediction = (random.randint(0,100))
+    if i >= 5 and i <= 19:
+        if prediction >= 60 and prediction <=89:
+            temp += 3
+        elif prediction >=30 and prediction <=59:
+            temp += 2
+        elif prediction >=90:
+            temp += 4
+        else:
+            temp -= 1
+    else:
+        if prediction >= 50:
+            temp -= 1
+        else:
+            temp -= 2
+    print("TEMPERATURE", temp)
     weather_forecast.append(temp)
-    time=(random.randint(0,23))
-    weather_forecast_size = len(weather_forecast)
+    time_forecast.update({ i : weather_forecast[-1]})
     sleep(0.1)
-    time_forecast.update( {weather_forecast[-1] : time})
+
+#see tagurpidi time_forecast.update({ i : weather_forecast[-1]}) lubab sorteerida kraadidega
+sorted_time_forecast = dict(sorted(time_forecast.items()))
+print(sorted_time_forecast)
+for key, value in sorted_time_forecast.items():
+    print("The temperature was", value, "degrees at",key, "o'clock")
+
+print(sorted_time_forecast)
 
 
-for key, value in time_forecast.items():
-    print("The temperature was", key, "degrees at",value, "o'clock")
+
 
 
 
 while True:
-    question = int(input("How many degrees is it: "))
-    if question in weather_forecast:
+    question = int(input("What is today's highest temperature: "))
+    if question == max(weather_forecast):
         print("Yay correct")
         break
     else:
-        print("Guess again")
+        print("Wrong, guess again")
